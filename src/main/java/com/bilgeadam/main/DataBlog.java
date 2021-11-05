fb6eefa208d55ae4af4283f386b17a159e4f7a8b
hamitmizrak <hamitmizrak@gmail.com>
2021-11-05 17:53:21 +0300

    DataBlog test

---
+++ src/main/java/com/bilgeadam/main/DataBlog.java
@@ -0,0 +1,44 @@
+package com.bilgeadam.main;
+
+public class DataBlog {
+	
+	public static void main(String[] args) {
+		
+		// ekleme
+		// AdminDto adminDto = new AdminDto();
+		// adminDto.setName("Hamit");
+		// adminDto.setSurName("Mızrak");
+		// adminDto.setTelNumber("44444");
+		// adminDto.setEmailAddress("hamitmizrak@gmail.com");
+		// adminDto.setPassword("şifre");
+		//
+		// AdminController adminController = new AdminController();
+		// adminController.insert(adminDto);
+		
+		// güncelleme
+		// AdminDto adminDto2 = new AdminDto();
+		// adminDto2.setName("Hamit11");
+		// adminDto2.setSurName("Mızrak11");
+		// adminDto2.setTelNumber("11");
+		// adminDto2.setEmailAddress("hamitmizrak11@gmail.com");
+		// adminDto2.setPassword("şifre11");
+		// adminDto2.setId(12);
+		//
+		// AdminController adminController2 = new AdminController();
+		// adminController2.update(adminDto2);
+		
+		// silme
+		// AdminDto adminDto4 = new AdminDto();
+		// adminDto4.setId(5);
+		//
+		// AdminController adminController4 = new AdminController();
+		// adminController4.delete(adminDto4);
+		
+		// listelemek
+		// AdminController adminDao5 = new AdminController();
+		// ArrayList<AdminDto> temp = adminDao5.list();
+		// temp.forEach(System.out::println);
+		
+	}
+	
+}
