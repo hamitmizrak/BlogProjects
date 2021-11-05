604c922b8f9846130b800350be8fb66ee9fa3d7d
hamitmizrak <hamitmizrak@gmail.com>
2021-11-05 17:52:57 +0300

    controller eklendi

---
+++ src/main/java/com/bilgeadam/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.bilgeadam.controller;
+
+import java.util.ArrayList;
+
+import com.bilgeadam.dao.IDAOImplements;
+import com.bilgeadam.dto.UserDto;
+
+public class UserController implements IDAOImplements<UserDto> {
+	
+	// değişkenlerimiz
+	
+	@Override
+	public void insert(UserDto t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void update(UserDto t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void delete(UserDto t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public ArrayList<UserDto> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
